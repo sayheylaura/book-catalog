528e3d712cacde062b0c836a7997cb7bdc2c0c9d
Laura Ferrando <lauraferrandof@gmail.com>
2019-03-16 20:24:06 +0100

    Add link to logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import logo from '../../images/logo.svg';
 import './Logo.scss';
+import { Link } from 'react-router-dom';
 
 const Logo = ({ size }) => {
-  return (
-    <div className={`logo__container ${size}`}>
-      <img src={logo} alt="Logo" className="logo__image"></img>
-    </div>
-  );
+	return (
+		<Link to="/">
+			<div className={`logo__container ${size}`}>
+				<img src={logo} alt="Logo" className="logo__image"></img>
+			</div>
+		</Link>
+	);
 }
 
 Logo.propTypes = {
-  size: PropTypes.string.isRequired
+	size: PropTypes.string.isRequired
 }
 
 export default Logo;
