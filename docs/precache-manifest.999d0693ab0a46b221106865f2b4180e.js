d6f15ab50760934d70a67a1150e96deb2ae6770c
Laura Ferrando <lauraferrandof@gmail.com>
2019-03-18 19:38:08 +0100

    Create docs folder

---
+++ docs/precache-manifest.999d0693ab0a46b221106865f2b4180e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a20316ac76a98f19248fec5cffde8d",
+    "url": "./static/media/logo.b6a20316.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1777de662fc8db84293e",
+    "url": "./static/js/main.c4a7ccd8.chunk.js"
+  },
+  {
+    "revision": "6f009734923c5141c795",
+    "url": "./static/js/2.94e5db96.chunk.js"
+  },
+  {
+    "revision": "1777de662fc8db84293e",
+    "url": "./static/css/main.6eefd4ca.chunk.css"
+  },
+  {
+    "revision": "de6bab7096cf01e86ceb2dcfe6e50386",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e824a7c896e7aad1a9d4a99468afc39.js"
+  "./precache-manifest.999d0693ab0a46b221106865f2b4180e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
